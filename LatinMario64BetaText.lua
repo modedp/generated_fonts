5d91e06faa5c39df863fd33ea6acab4820174981
modedp <155116091+modedp@users.noreply.github.com>
2025-03-04 06:45:37 +0000

    Update LatinMario64BetaText.lua

--- LatinMario64BetaText.lua
+++ LatinMario64BetaText.lua
@@ -28,32 +28,9 @@ module.atlases = {
 	[18] = "rbxassetid://103989708134203";
 };
 
-local module = {}
-
-module.atlases = {
-	[1] = "rbxassetid://";
-	[2] = "rbxassetid://";
-	[3] = "rbxassetid://";
-	[4] = "rbxassetid://";
-	[5] = "rbxassetid://";
-	[6] = "rbxassetid://";
-	[7] = "rbxassetid://";
-	[8] = "rbxassetid://";
-	[9] = "rbxassetid://";
-	[10] = "rbxassetid://";
-	[11] = "rbxassetid://";
-	[12] = "rbxassetid://";
-	[13] = "rbxassetid://";
-	[14] = "rbxassetid://";
-	[15] = "rbxassetid://";
-	[16] = "rbxassetid://";
-	[17] = "rbxassetid://";
-	[18] = "rbxassetid://";
-};
-
 module.font = {
 	information = {
-		family = "LatinJP-SM64 1.1",
+		family = "LatinMario64Beta",
 		styles = {"Regular"},
 		sizes = {100, 98, 96, 94, 92, 90, 88, 86, 84, 82, 80, 78, 76, 74, 72, 70, 68, 66, 64, 62, 60, 58, 56, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 11, 10, 9, 8},
 		useEnums = false
